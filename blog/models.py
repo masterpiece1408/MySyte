a62ad65c2f97f357872d8e5c1aa5386305ad3167
Gyalla <Gyallahorn@gmail.com>
2020-01-15 21:20:17 +0900

    added transactions

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,19 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Cars(models.Model):
+    seller = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    classOfCar = models.CharField(max_length=30)
+    mark= models.CharField(max_length=30)
+    model = models.CharField(max_length=50)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
