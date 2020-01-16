0c8bee7e0836d62681528ecb9c161f68f9d3d0cc
Gyalla <Gyallahorn@gmail.com>
2020-01-16 17:08:03 +0900

    its a stable version

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,9 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,null=True)
     title = models.CharField(max_length=200)
+    classOfCar = models.CharField(max_length=200)
     text = models.TextField()
+    
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
@@ -19,7 +21,7 @@ class Post(models.Model):
 
 class Cars(models.Model):
     seller = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    classOfCar = models.CharField(max_length=30)
+    # classOfCar = models.CharField(max_length=30)
     mark= models.CharField(max_length=30)
     model = models.CharField(max_length=50)
     text = models.TextField()
